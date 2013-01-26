cef6796d0154308b99d68bd7f960b9407c5f92fb
Stanislav [spoilt_exile] Nepochatov <spoilt.exile@gmail.com>
2013-01-26 22:47:15 +0200

    Copyright and license information modification;

--- src/ribbonlastmessage/RibbonLastMessage.java
+++ src/ribbonlastmessage/RibbonLastMessage.java
@@ -1,12 +1,23 @@
-/*
- * This code is distributed under terms of GNU GPLv2.
- * *See LICENSE file.
- * ©UKRINFORM 2011-2012
- */
+/**
+ * This file is part of RibbonLastMessage application (check README).
+ * Copyright (C) 2012-2013 Stanislav Nepochatov
+ * 
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+**/
 
 package ribbonlastmessage;
-
-import java.io.IOException;
 
 /**
  * Main class
@@ -130,7 +141,7 @@ public class RibbonLastMessage {
         /**
          * Make current thread wait for status returning.
          */
-        public void waitForOK() throws IOException {
+        public void waitForOK() throws java.io.IOException {
             String returnedStatus;
             while (networkIsUp) {
                 if ((returnedStatus = inStream.readLine()).equals("OK:")) {
